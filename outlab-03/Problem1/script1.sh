15aaa5c8b80710426ecc602c1bb658920587f88d
krsrv <arch@crunk.localdomain>
2017-08-04 23:04:11 +0530

    Problem 1 corrected

--- outlab-03/Problem1/script1.sh
+++ outlab-03/Problem1/script1.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-awk 'NR==1 { printf "%s\t%s\t%s\t%s\tPoints\n",$1,$2,$3,$4} NR>1 {printf "%s\t%d\t%d\t%d\t%d\n",$1,$2,$3,$4,4*$2+2*$4}' $1
+awk '
+BEGIN {
+# FS="[[:space:]]+";
+}
+NR==1 {
+    printf "%s\t%s\t%s\t%s\tPoints\n",$1,$2,$3,$4
+} 
+NR>1 {
+    printf "%s\t%d\t%d\t%d\t%d\n",$1,$2,$3,$4,4*$3+2*$4
+}' $1
