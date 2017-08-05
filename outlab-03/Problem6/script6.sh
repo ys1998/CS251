094e97815d406ebc18028a2183c194ba1efcb70d
ys1998 <yashshah2398@gmail.com>
2017-08-05 22:06:57 +0530

    Modified script6

--- outlab-03/Problem6/script6.sh
+++ outlab-03/Problem6/script6.sh
@@ -10,10 +10,10 @@ cat $3 | sed -e '/^\s*$/d' -e 's/^\s*//' -e 's/\s*$//' -e 's/\s\s*/\n/g' | sed -
 # merge the three files
 cat out1 >> out2
 cat out2 >> out3
-
+total=$( awk 'BEGIN { total=0; } { if($0!=""){ total=total+$1; }} END { print total }' out3 )
 # find total word count using awk
-cat out3 | sort --key=2,2 | awk 'BEGIN { cnt=0; word=""; OFS="\t"} \
+cat out3 | sort --key=2,2 | awk -v tot=$total 'BEGIN { t=tot; OFS=","; cnt=0; word="";} \
 	{ if($2!=word){\
-		print cnt,word; word=$2; cnt=$1; }\
-	else{ cnt=cnt+$1; }} END { print cnt,word;}' | sed -e '/^0\s*$/d'
+		print word,cnt/t; word=$2; cnt=$1; }\
+	else{ cnt=cnt+$1; }} END { print word,cnt/t;}' | sed '/^,0$/d'
 rm out1 out2 out3
