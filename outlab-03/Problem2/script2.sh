02526f8c4a3750568816a4b8229243a40360f240
krsrv <arch@crunk.localdomain>
2017-08-05 17:31:37 +0530

    Problem 4 updated

--- outlab-03/Problem2/script2.sh
+++ outlab-03/Problem2/script2.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-sed -E 's/[[:space:]][[:space:]]*/ /g' $1 | sed 's/^[[:space:]][[:space:]]*//g' | sed 's/[[:space:]][[:space:]]*$//g' | sed '/^[[:space:]]*$/d'
+sed '/^[[:space:]]*$/d' $1 | awk '
+BEGIN {
+    FS = "\n";
+    RS = "\"";
+    OFS = "\n";
+    ORS = "\"";
+    bracket = 1;
+}
+
+($0 ~ /^[[:space:]]*$/) { bracket = 2; print" ";}
+
+($0 !~ /^[[:space:]]*$/) {
+    for(i = 1; i <= NF; i++){
+	gsub(/[[:space:]][[:space:]]*/, " ", $i);
+	gsub(/^[[:space:]]*/, "", $i);
+	gsub(/[[:space:]]*$/, "", $i);
+	gsub(/\([[:punct:]]\)[[:space:]]\([[:alnum:]]\)/, "\1\2", $i);
+    }
+    if(bracket == 0){
+	bracket = 1;
+    }
+    else if(bracket == 1){
+	$0 = " " $0 " ";
+	bracket = 0;
+    }
+    else{
+	bracket = 1;
+    }
+    print $0;
+}' | sed -e 's/^[[:space:]]//' -e '$s/[[:space:]]\"$//'
