6f37250e305f9af40ccfa028bfe5c75525d84c1c
ys1998 <yashshah2398@gmail.com>
2017-08-19 21:30:48 +0530

    Modified Problem 3

--- outlab-05/lab5_160050056_160050057_160050002/Problem3.java
+++ outlab-05/lab5_160050056_160050057_160050002/Problem3.java
@@ -10,85 +10,86 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Problem3{
-	private static void get(String args) throws IOException{
+public class Problem3 {
 
-		String host = "https://www.cse.iitb.ac.in";
-		String path = "/~safeer/get_hash.php";
-		String query = "?input=" + args;
-		
-		//Much better way to obtain the host address
-		//String urlString = host + path; 
-		//URL url = new URL(urlString);
-		//url.getHost(); instead of "www.cse.iitb.ac.in"
+    private static void get(String args) throws IOException {
 
-		SSLSocketFactory factory = (SSLSocketFactory)SSLSocketFactory.getDefault();
-		SSLSocket socket = (SSLSocket) factory.createSocket("www.cse.iitb.ac.in", 443);
-		
-		//Or, in short:
-		//SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket("www.cse.iitb.ac.in", 443);
-		
-		//This won't work:
-		//Socket socket = new Socket("www.cse.iitb.ac.in", 80);
+        String host = "https://www.cse.iitb.ac.in";
+        String path = "/~safeer/get_hash.php";
+        String query = "?input=" + args;
 
-		PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-		out.println("GET " + path + query + " HTTP/1.0"); 
-		out.println("Host: www.cse.iitb.ac.in");
-		out.println();
-		out.flush();
+        //Much better way to obtain the host address
+        //String urlString = host + path; 
+        //URL url = new URL(urlString);
+        //url.getHost(); instead of "www.cse.iitb.ac.in"
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) factory.createSocket("www.cse.iitb.ac.in", 443);
 
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(
-					socket.getInputStream()));
+        //Or, in short:
+        //SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket("www.cse.iitb.ac.in", 443);
+        //This won't work:
+        //Socket socket = new Socket("www.cse.iitb.ac.in", 80);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        out.println("GET " + path + query + " HTTP/1.0");
+        out.println("Host: www.cse.iitb.ac.in");
+        out.println();
+        out.flush();
 
-		String line;
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        socket.getInputStream()));
 
-		while ((line = in.readLine()) != null) {
-			if(line.length() > 5 && line.substring(0,4).equals("Hash")) System.out.println(line);
-		}
+        String line;
 
-		out.close();
-		in.close();
-	}
-	
-	private static void post(String args) throws IOException{
+        while ((line = in.readLine()) != null) {
+            if (line.length() > 5 && line.substring(0, 4).equals("Hash")) {
+                System.out.println(line);
+            }
+        }
 
-		String host = "https://www.cse.iitb.ac.in";
-		String path = "/~safeer/post_hash.php";
-		String query = "input=" + args;
+        out.close();
+        in.close();
+    }
 
-		SSLSocketFactory factory = (SSLSocketFactory)SSLSocketFactory.getDefault();
-		SSLSocket socket = (SSLSocket) factory.createSocket("www.cse.iitb.ac.in", 443);
-		
-		PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-		out.println("POST " + path + " HTTP/1.0");
-		out.println("Host: www.cse.iitb.ac.in");
-		out.println("Content-Type: application/x-www-form-urlencoded");
-		out.println("Content-Length: " + query.length()); 
-		out.println();
-		out.println(query);
-		//out.println();
-		out.flush();
+    private static void post(String args) throws IOException {
 
-		BufferedReader in = new BufferedReader(
-				new InputStreamReader(
-					socket.getInputStream()));
+        String host = "https://www.cse.iitb.ac.in";
+        String path = "/~rohitrango/post.php";
+        String query = "input=" + args;
 
-		String line;
+        SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        SSLSocket socket = (SSLSocket) factory.createSocket("www.cse.iitb.ac.in", 443);
 
-		while ((line = in.readLine()) != null) {
-			if(line.length() > 5 && line.substring(0,4).equals("Hash")) System.out.println(line);
-		}
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        out.println("POST " + path + " HTTP/1.0");
+        out.println("Host: www.cse.iitb.ac.in");
+        out.println("Content-Type: application/x-www-form-urlencoded");
+        out.println("Content-Length: " + query.length());
+        out.println();
+        out.println(query);
+        //out.println();
+        out.flush();
 
-		out.close();
-		in.close();
-	}
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        socket.getInputStream()));
 
-	public static void main(String[] args) throws IOException{
-		System.out.println("Sending GET request: ");
-		get(args[0]);
-		System.out.println("Sending POST request: ");
-		post(args[0]);
-	}
+        String line, prev_line = "";
+
+        while ((line = in.readLine()) != null) {
+            // to get the last line of the response
+            prev_line = line;
+        }
+        System.out.println("Hash is : " + prev_line);
+        out.close();
+        in.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Sending GET request: ");
+        get(args[0]);
+        System.out.println("Sending POST request: ");
+        post(args[0]);
+    }
 
 }
