2fadd36ca71b594ce93ca2731e239d8b21840a43
ys1998 <yashshah2398@gmail.com>
2017-08-19 17:40:52 +0530

    Generalized Q1 to work with Q4

--- outlab-05/lab5_160050056_160050057_160050002/Problem1.java
+++ outlab-05/lab5_160050056_160050057_160050002/Problem1.java
@@ -4,6 +4,7 @@
 //https://www.tutorialspoint.com/apache_poi_word/apache_poi_word_text_extraction.htm
 //https://stackoverflow.com/questions/7488643/how-to-convert-comma-separated-string-to-arraylist
 //https://stackoverflow.com/questions/3571945/find-if-a-string-is-present-in-an-array
+
 import java.io.IOException;
 import java.io.File;
 import java.io.FileInputStream;
@@ -28,86 +29,96 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class Problem1{
-	public static void main(String args[]) throws Exception{
-		String defStopwords = "and,the,or,is,in,at,of,her,him,his";
-		if(args[0].endsWith("doc") || args[0].endsWith("docx")){	parse.extract(doc.main(args[0]) , defStopwords);}
-		else if( args[0].endsWith("xlsx") || args[0].endsWith("xls") ){	parse.extract(workbook.main(args[0]), defStopwords);}
-		else if(args[0].endsWith("pdf") ){		parse.extract(pdf.main(args[0]), defStopwords);}
-		else return;
-	}
+public class Problem1 {
+
+    public static void getCount(String filepath, String defStopwords) throws Exception {
+        if (filepath.endsWith("doc") || filepath.endsWith("docx")) {
+            parse.extract(doc.main(filepath), defStopwords);
+        } else if (filepath.endsWith("xlsx") || filepath.endsWith("xls")) {
+            parse.extract(workbook.main(filepath), defStopwords);
+        } else if (filepath.endsWith("pdf")) {
+            parse.extract(pdf.main(filepath), defStopwords);
+        } else {
+            return;
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        getCount(args[0], "and,the,or,is,in,at,of,her,him,his");
+    }
 }
 
 class parse {
-        public static void extract(String text, String listofstopword){
-               ArrayList<String> arr = new ArrayList<String> ();             
 
-                //text = text.replaceAll("[\n\r]", "");
-                String[] result = text.split("\\p{Punct}|\\s+");
-                List<String> stopwords = Arrays.asList(listofstopword.split(","));
-                for (String S : result){
-                	S = S.toLowerCase();
-			
-                        if(S.trim().equals("") || stopwords.contains(S))        continue;
-                        	//System.out.println(S);
-                                arr.add(S);
-                }
-                Collections.sort(arr);
-                SortedSet<String> uniq = new TreeSet<String>(arr);
-                //for(String S : arr){
-                for(String S : uniq){
-                        //if(S.trim().equals("")) continue;                
-                //for(String S : count)        
-                        System.out.println(S + " " + Collections.frequency(arr, S) );
-                }
+    public static void extract(String text, String listofstopword) {
+        ArrayList<String> arr = new ArrayList<String>();
+
+        //text = text.replaceAll("[\n\r]", "");
+        String[] result = text.split("\\p{Punct}|\\s+");
+        List<String> stopwords = Arrays.asList(listofstopword.split(","));
+        for (String S : result) {
+            S = S.toLowerCase();
+
+            if (S.trim().equals("") || stopwords.contains(S)) {
+                continue;
+            }
+            //System.out.println(S);
+            arr.add(S);
         }
+        Collections.sort(arr);
+        SortedSet<String> uniq = new TreeSet<String>(arr);
+        //for(String S : arr){
+        for (String S : uniq) {
+            //if(S.trim().equals("")) continue;                
+            //for(String S : count)        
+            System.out.println(S + " " + Collections.frequency(arr, S));
+        }
+    }
 }
 
 class doc {
-   public static String main(String args)throws Exception {
-      XWPFDocument docx = new XWPFDocument(new FileInputStream(args));
-      //using XWPFWordExtractor Class
-      XWPFWordExtractor we = new XWPFWordExtractor(docx);
-      String text;
-      text = we.getText();
-      we.close();
-      return text;
-   }
+
+    public static String main(String args) throws Exception {
+        XWPFDocument docx = new XWPFDocument(new FileInputStream(args));
+        //using XWPFWordExtractor Class
+        XWPFWordExtractor we = new XWPFWordExtractor(docx);
+        String text;
+        text = we.getText();
+        we.close();
+        return text;
+    }
 }
 
-class workbook 
-{
-   static XSSFRow row;
-   static String text;
-   public static String main(String args) throws Exception 
-   {
-      FileInputStream fis = new FileInputStream(
-      new File(args));
-      XSSFWorkbook workbook = new XSSFWorkbook(fis);
-      XSSFSheet spreadsheet = workbook.getSheetAt(0);
-      Iterator < Row > rowIterator = spreadsheet.iterator();
-      while (rowIterator.hasNext()) 
-      {
-         row = (XSSFRow) rowIterator.next();
-         Iterator < Cell > cellIterator = row.cellIterator();
-         while ( cellIterator.hasNext()) 
-         {
-            Cell cell = cellIterator.next();
-            switch (cell.getCellTypeEnum()) 
-            {
-               case NUMERIC:
-               text = cell.getNumericCellValue() + "," + text;
-               break;
-               case STRING:
-               text = cell.getStringCellValue() + "," + text;
-               break;
+class workbook {
+
+    static XSSFRow row;
+    static String text;
+
+    public static String main(String args) throws Exception {
+        FileInputStream fis = new FileInputStream(
+                new File(args));
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet spreadsheet = workbook.getSheetAt(0);
+        Iterator< Row> rowIterator = spreadsheet.iterator();
+        while (rowIterator.hasNext()) {
+            row = (XSSFRow) rowIterator.next();
+            Iterator< Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                switch (cell.getCellTypeEnum()) {
+                    case NUMERIC:
+                        text = cell.getNumericCellValue() + "," + text;
+                        break;
+                    case STRING:
+                        text = cell.getStringCellValue() + "," + text;
+                        break;
+                }
             }
-         }
-         
-      }
-      fis.close();
-      return text;
-   }
+
+        }
+        fis.close();
+        return text;
+    }
 }
 
 /*
@@ -134,25 +145,25 @@ public class pdf {
       }
    }
 }
-*/
+ */
+class pdf {
 
-class pdf{
-   public static String main(String args) throws IOException {
+    public static String main(String args) throws IOException {
 
-      //Loading an existing document
-      File file = new File(args	);
-      PDDocument document = PDDocument.load(file);
+        //Loading an existing document
+        File file = new File(args);
+        PDDocument document = PDDocument.load(file);
 
-      //Instantiate PDFTextStripper class
-      PDFTextStripper pdfStripper = new PDFTextStripper();
+        //Instantiate PDFTextStripper class
+        PDFTextStripper pdfStripper = new PDFTextStripper();
 
-      //Retrieving text from PDF document
-      String text = pdfStripper.getText(document);
-      
-      //Closing the document
-      document.close();
+        //Retrieving text from PDF document
+        String text = pdfStripper.getText(document);
 
-      return text;
+        //Closing the document
+        document.close();
 
-   }
+        return text;
+
+    }
 }
