21a5703b13dc103c3e2b6560f83143bade26d710
ys1998 <yashshah2398@gmail.com>
2017-08-19 19:33:43 +0530

    Modified Q1 and Q4

--- outlab-05/lab5_160050056_160050057_160050002/Problem1.java
+++ outlab-05/lab5_160050056_160050057_160050002/Problem1.java
@@ -4,6 +4,7 @@
 //https://www.tutorialspoint.com/apache_poi_word/apache_poi_word_text_extraction.htm
 //https://stackoverflow.com/questions/7488643/how-to-convert-comma-separated-string-to-arraylist
 //https://stackoverflow.com/questions/3571945/find-if-a-string-is-present-in-an-array
+
 import java.io.IOException;
 import java.io.File;
 import java.io.FileInputStream;
@@ -23,92 +24,99 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class Problem1{
-	public static void main(String args[]) throws Exception{
-		String defStopwords = "and,the,or,is,in,at,of,her,him,his";
-		Map<String, Integer> table;
-		if(args[0].endsWith("doc") || args[0].endsWith("docx")){	table  = parse.extract(doc.main(args[0]) , defStopwords);}
-		else if( args[0].endsWith("xlsx") || args[0].endsWith("xls") ){	table = parse.extract(workbook.main(args[0]), defStopwords);}
-		else if(args[0].endsWith("pdf") ){		table = parse.extract(pdf.main(args[0]), defStopwords);}
-		else return;
-		for (Map.Entry<String, Integer> entry : table.entrySet()){
-			    System.out.println(entry.getKey() + " " + entry.getValue());
-			}
-	}
+public class Problem1 {
+
+    public static void main(String args[]) throws Exception {
+        String defStopwords = "and,the,or,is,in,at,of,her,him,his";
+        Map<String, Integer> table;
+        if (args[0].endsWith("doc") || args[0].endsWith("docx")) {
+            table = parse.extract(doc.main(args[0]), defStopwords);
+        } else if (args[0].endsWith("xlsx") || args[0].endsWith("xls")) {
+            table = parse.extract(workbook.main(args[0]), defStopwords);
+        } else if (args[0].endsWith("pdf")) {
+            table = parse.extract(pdf.main(args[0]), defStopwords);
+        } else {
+            return;
+        }
+        for (Map.Entry<String, Integer> entry : table.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
 }
 
 class parse {
-        public static Map<String,Integer> extract(String text, String listofstopword){
-               ArrayList<String> arr = new ArrayList<String> ();             
 
-                //text = text.replaceAll("[\n\r]", "");
-                String[] result = text.split("\\p{Punct}|\\s+");
-                List<String> stopwords = Arrays.asList(listofstopword.split(","));
-                for (String S : result){
-                	S = S.toLowerCase();
-			
-                        if(S.trim().equals("") || stopwords.contains(S))        continue;
-                        	//System.out.println(S);
-                                arr.add(S);
-                }
-                //Collections.sort(arr);
-                Map<String, Integer> table = new TreeMap<String, Integer>();                
-		//for(String S : arr){
-                for(String S : arr){
-                        //if(S.trim().equals("")) continue;                
-                //for(String S : count)        
-			table.put(S,Collections.frequency(arr,S));                        
-			//System.out.println(S + " " + Collections.frequency(arr, S) );
-                }
-		return table;
+    public static Map<String, Integer> extract(String text, String listofstopword) {
+        ArrayList<String> arr = new ArrayList<String>();
+
+        //text = text.replaceAll("[\n\r]", "");
+        String[] result = text.split("\\p{Punct}|\\s+");
+        List<String> stopwords = Arrays.asList(listofstopword.split(","));
+        for (String S : result) {
+            S = S.toLowerCase();
+
+            if (S.trim().equals("") || stopwords.contains(S)) {
+                continue;
+            }
+            //System.out.println(S);
+            arr.add(S);
         }
+        //Collections.sort(arr);
+        Map<String, Integer> table = new TreeMap<String, Integer>();
+        //for(String S : arr){
+        for (String S : arr) {
+            //if(S.trim().equals("")) continue;                
+            //for(String S : count)        
+            table.put(S, Collections.frequency(arr, S));
+            //System.out.println(S + " " + Collections.frequency(arr, S) );
+        }
+        return table;
+    }
 }
 
 class doc {
-   public static String main(String args)throws Exception {
-      XWPFDocument docx = new XWPFDocument(new FileInputStream(args));
-      //using XWPFWordExtractor Class
-      XWPFWordExtractor we = new XWPFWordExtractor(docx);
-      String text;
-      text = we.getText();
-      we.close();
-      return text;
-   }
+
+    public static String main(String args) throws Exception {
+        XWPFDocument docx = new XWPFDocument(new FileInputStream(args));
+        //using XWPFWordExtractor Class
+        XWPFWordExtractor we = new XWPFWordExtractor(docx);
+        String text;
+        text = we.getText();
+        we.close();
+        return text;
+    }
 }
 
-class workbook 
-{
-   static XSSFRow row;
-   static String text;
-   public static String main(String args) throws Exception 
-   {
-      FileInputStream fis = new FileInputStream(
-      new File(args));
-      XSSFWorkbook workbook = new XSSFWorkbook(fis);
-      XSSFSheet spreadsheet = workbook.getSheetAt(0);
-      Iterator < Row > rowIterator = spreadsheet.iterator();
-      while (rowIterator.hasNext()) 
-      {
-         row = (XSSFRow) rowIterator.next();
-         Iterator < Cell > cellIterator = row.cellIterator();
-         while ( cellIterator.hasNext()) 
-         {
-            Cell cell = cellIterator.next();
-            switch (cell.getCellTypeEnum()) 
-            {
-               case NUMERIC:
-               text = cell.getNumericCellValue() + "," + text;
-               break;
-               case STRING:
-               text = cell.getStringCellValue() + "," + text;
-               break;
+class workbook {
+
+    static XSSFRow row;
+    static String text;
+
+    public static String main(String args) throws Exception {
+        FileInputStream fis = new FileInputStream(
+                new File(args));
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet spreadsheet = workbook.getSheetAt(0);
+        Iterator< Row> rowIterator = spreadsheet.iterator();
+        while (rowIterator.hasNext()) {
+            row = (XSSFRow) rowIterator.next();
+            Iterator< Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                switch (cell.getCellTypeEnum()) {
+                    case NUMERIC:
+                        text = cell.getNumericCellValue() + "," + text;
+                        break;
+                    case STRING:
+                        text = cell.getStringCellValue() + "," + text;
+                        break;
+                }
             }
-         }
-         
-      }
-      fis.close();
-      return text;
-   }
+
+        }
+        fis.close();
+        return text;
+    }
 }
 
 /*
@@ -135,25 +143,25 @@ public class pdf {
       }
    }
 }
-*/
+ */
+class pdf {
 
-class pdf{
-   public static String main(String args) throws IOException {
+    public static String main(String args) throws IOException {
 
-      //Loading an existing document
-      File file = new File(args	);
-      PDDocument document = PDDocument.load(file);
+        //Loading an existing document
+        File file = new File(args);
+        PDDocument document = PDDocument.load(file);
 
-      //Instantiate PDFTextStripper class
-      PDFTextStripper pdfStripper = new PDFTextStripper();
+        //Instantiate PDFTextStripper class
+        PDFTextStripper pdfStripper = new PDFTextStripper();
 
-      //Retrieving text from PDF document
-      String text = pdfStripper.getText(document);
-      
-      //Closing the document
-      document.close();
+        //Retrieving text from PDF document
+        String text = pdfStripper.getText(document);
 
-      return text;
+        //Closing the document
+        document.close();
 
-   }
+        return text;
+
+    }
 }
--- outlab-05/lab5_160050056_160050057_160050002/Problem4.java
+++ outlab-05/lab5_160050056_160050057_160050002/Problem4.java
@@ -1,89 +1,118 @@
 //https://stackoverflow.com/questions/37424284/unreported-exception-java-lang-exception-must-be-caught-or-declared-to-be-throw
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.io.File;
 import java.awt.event.*;
 import java.awt.GridLayout;
+import java.util.Map;
 
-class Form{
-	private JFrame frame;
-	private JButton openButton;
-	private JButton goButton;
-	private JButton resetButton;
-	private JTextField file;
-	private JTextField stopword;
-	private JLabel output;
-	private JLabel text1;
-	private JPanel row1;
-	String defStopWord="and,the,or,is,in,at,of,her,him,his";
-	final JFileChooser fc = new JFileChooser();
-	File filepath;
-	String arg;
-	class ButtonClick implements ActionListener{
-		//haven't assigned the return value from parse to any return variable yet.
-		public void main() {
-			try {   arg=filepath.getCanonicalPath();        			
-				if(arg.endsWith("doc") || arg.endsWith("docx")){	parse.extract(doc.main(arg) , stopword.getText());}
-				else if( arg.endsWith("xlsx") || arg.endsWith("xls") ){	parse.extract(workbook.main(arg), stopword.getText());}
-				else if(arg.endsWith("pdf") ){		parse.extract(pdf.main(arg), stopword.getText());}
-			}catch (Exception e) {e.printStackTrace();}
-		}
-		@Override
-		public void actionPerformed(ActionEvent e){
-			if (e.getSource() == openButton) {
-			        int returnVal = fc.showOpenDialog(frame);
+class Form {
 
-		        	if (returnVal == JFileChooser.APPROVE_OPTION) {
-       			     		filepath = fc.getSelectedFile();
-				}
-        		}
-			String command=e.getActionCommand();
-			if(command=="Go"){
-				output.setText("Opening and parsing file...");
-				main();
-	
-			}
-			else if(command=="reset"){
-				file.setText("");
-				stopword.setText("and,the,or,is,in,at,of,her,him,his");				
-				//filepath  = /home;
-				output.setText("Fields reset.");
-			}
-		}
-	}
-	public Form(){
-		frame=new JFrame("Problem 4");
-		openButton=new JButton("open");		
-		goButton=new JButton("Go");
-		resetButton=new JButton("Reset");
-		file=new JTextField(20);
-		stopword=new JTextField(20);
-		output=new JLabel("Please select file and enter stopwords");
-		text1=new JLabel("Stopwords");
-		row1=new JPanel();
-		row1.add(text1); row1.add(stopword);
-		stopword.setText("and,the,or,is,in,at,of,her,him,his");
-		frame.setSize(600,300);
-		frame.setLayout(new GridLayout(8,0));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		frame.add(row1);
-		frame.add(openButton);
-		frame.add(goButton);
-		frame.add(resetButton);
-		frame.add(output);
+    private JFrame frame;
+    private JButton openButton;
+    private JButton goButton;
+    private JButton resetButton;
+    private JTextField file;
+    private JTextField stopword;
+    private JLabel output;
+    private JLabel text1;
+    private JPanel row1;
+    private JTable result;
+    private Map<String,Integer> result_map;
+    String defStopWord = "and,the,or,is,in,at,of,her,him,his";
+    final JFileChooser fc = new JFileChooser();
+    File filepath;
+    String arg;
 
-		resetButton.setActionCommand("reset");
-		openButton.addActionListener(new ButtonClick());
-		goButton.addActionListener(new ButtonClick());
-		resetButton.addActionListener(new ButtonClick());
+    class ButtonClick implements ActionListener {
+        //haven't assigned the return value from parse to any return variable yet.
 
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}
+        public void main() {
+            try {
+                arg = filepath.getCanonicalPath();
+                if (arg.endsWith("doc") || arg.endsWith("docx")) {
+                    result_map=parse.extract(doc.main(arg), stopword.getText());
+                } else if (arg.endsWith("xlsx") || arg.endsWith("xls")) {
+                    result_map=parse.extract(workbook.main(arg), stopword.getText());
+                } else if (arg.endsWith("pdf")) {
+                    result_map=parse.extract(pdf.main(arg), stopword.getText());
+                }
+                frame.remove(result);               // TABLE
+                DefaultTableModel temp=new DefaultTableModel();
+                temp.addColumn("Word"); temp.addColumn("Frequency");
+                for(Map.Entry<String,Integer> entry:result_map.entrySet()){
+                    temp.addRow(new Object[]{entry.getKey(),entry.getValue()});
+                }
+                result=new JTable(temp);
+                frame.add(result);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == openButton) {
+                int returnVal = fc.showOpenDialog(frame);
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    filepath = fc.getSelectedFile();
+                }
+            }
+            String command = e.getActionCommand();
+            if (command == "Go") {
+                output.setText("Opening and parsing file...");
+                main();
+
+            } else if (command == "reset") {
+                file.setText("");
+                stopword.setText("and,the,or,is,in,at,of,her,him,his");
+                //filepath  = /home;
+                output.setText("Fields reset.");
+            }
+        }
+    }
+
+    public Form() {
+        frame = new JFrame("Problem 4");
+        openButton = new JButton("open");
+        goButton = new JButton("Go");
+        resetButton = new JButton("Reset");
+        file = new JTextField(20);
+        stopword = new JTextField(20);
+        output = new JLabel("Please select file and enter stopwords");
+        text1 = new JLabel("Stopwords");
+        row1 = new JPanel();
+        result=new JTable();                // TABLE
+        row1.add(text1);
+        row1.add(stopword);
+        stopword.setText("and,the,or,is,in,at,of,her,him,his");
+        frame.setSize(600, 300);
+        frame.setLayout(new GridLayout(8, 0));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(row1);
+        frame.add(openButton);
+        frame.add(goButton);
+        frame.add(resetButton);
+        frame.add(output);
+        frame.add(result);              // TABLE
+
+        resetButton.setActionCommand("reset");
+        openButton.addActionListener(new ButtonClick());
+        goButton.addActionListener(new ButtonClick());
+        resetButton.addActionListener(new ButtonClick());
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 
 }
-public class Problem4{
-	public static void main(String[] args){
-		new Form();
-	}
+
+public class Problem4 {
+
+    public static void main(String[] args) {
+        new Form();
+    }
 }
