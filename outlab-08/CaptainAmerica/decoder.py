36c5864d40a3f746a8c928cf821b5d741e3dee87
ys1998 <yashshah2398@gmail.com>
2017-09-08 12:59:30 +0530

    Added Avengers and CaptainAmerica files

---
+++ outlab-08/CaptainAmerica/decoder.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def decode():
+	data=np.load('message.npy')
+	# data is RGB array
+	# org_img=plt.imshow(data)
+	# plt.show()
+	
+	# Contrast stretching is done on three channels together
+	c_r,d_r=np.min(data[:,:,0]),np.max(data[:,:,0])
+	c_g,d_g=np.min(data[:,:,1]),np.max(data[:,:,1])
+	c_b,d_b=np.min(data[:,:,2]),np.max(data[:,:,2])
+	nd=np.array(data)
+	nd[:,:,0]-=c_r;nd[:,:,1]-=c_g;nd[:,:,2]-=c_b;
+	nd[:,:,0]*=255/(d_r-c_r);nd[:,:,1]*=255/(d_g-c_g);nd[:,:,2]*=255/(d_b-c_b);
+	new_img=plt.imshow(nd)
+	plt.show()
+
+	# c=np.min(data); d=np.max(data)
+	# nd_2=np.array(data)
+	# nd_2-=c
+	# nd_2*=255/(d-c)
+	# new_img_2=plt.imshow(nd_2)
+	# plt.show()
+
+
+
+
+if __name__ == '__main__':
+	decode()
